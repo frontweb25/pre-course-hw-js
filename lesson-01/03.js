22e85efba27e8abc88dfabe677e1330ae0463154
Zhenya <ruban.z1@mail.ru>
2024-01-16 14:59:53 +0300

    add-tast-03

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,9 +1,12 @@
 
 let number = 1;
 console.log(number)
-console.log(number + ++number)
-console.log(number + --number)
-console.log(number--)
+number++
+console.log(++number)
+number--
+console.log(++number)
+number--
+console.log(--number)
 
 
 
