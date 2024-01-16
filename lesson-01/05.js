5f736ce3b7d36e29b88c2cd8a14d3c360e6a5ea2
Zhenya <ruban.z1@mail.ru>
2024-01-16 16:02:30 +0300

    dsgvsg

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -4,11 +4,11 @@ let rule = "Еще не родился тот человек который по
 switch (true) {
     case rule.length < 25:
       console.log("Все таки нет правил без исключения")
-      break
+      break;
     case rule.length > 25:
       console.log("У меня 100 пудов все получится")
-      break
+      break;
       case rule.length === 25:
       console.log("50 на 50")
-      break
+      break;
   }
