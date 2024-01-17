337fb57ca03f48489d940e0da28836ce4874d98e
Zhenya <ruban.z1@mail.ru>
2024-01-17 16:52:41 +0300

    cloneObj-spredOparetor

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -3,11 +3,7 @@ let passport = {
     surname: "Petrov",
 }
 
-let clone = {}
-
-for (let key in passport) {
-    clone[key] = passport[key]
-}
+let clone = {... passport}
 
 console.log(passport.name)
 console.log(clone.name = 'Ivan')
