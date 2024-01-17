1ec975453065e57c8265b1c889273378d32eaf13
Zhenya <ruban.z1@mail.ru>
2024-01-17 16:57:30 +0300

    copyObj

---
+++ lesson-02/06.js
@@ -0,0 +1,13 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let cloneObject = {...passportWithAddress}
+
+console.log(passportWithAddress.address.city)
+console.log(cloneObject.address.city = 'Bobryisk')
