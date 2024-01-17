b47221ec95361ebc0ffeae804cfd58b3357f2b8c
Zhenya <ruban.z1@mail.ru>
2024-01-17 16:50:00 +0300

    cloneObj

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -8,6 +8,6 @@ let clone = {}
 for (let key in passport) {
     clone[key] = passport[key]
 }
-clone.name = 'Ivan'
+
 console.log(passport.name)
-console.log(clone.name)
+console.log(clone.name = 'Ivan')
